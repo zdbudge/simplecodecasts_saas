2bd044401bd29f16d871cadf45015e70df776e92
Zhaque Budge <zdbudge@gmail.com>
2016-04-12 17:45:39 +0000

    Configured mailer and view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,9 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'zdbudge@gmail.com'
-    
-    def contact_email(name, email, body)
-        @name = name
-        @email = email
-        @body = body
-        
-        mail(from: email, subject: 'Contact Form Message')
-    end
+  default to: 'your@email_address.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
